99b740f7e22643168b048fb2ff7a85654f5e9b09
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-10-01 12:32:17 -0400

    add user function and routes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,22 @@ const About = () => {
 
             <div className="py-4">
                 <h1>About page</h1> 
+                <p className= "lead"> 
+                Hello" is a song by English singer-songwriter Adele, released on 23 October 2015 by XL Recordings
+                as the lead single from her third studio album, 25 (2015). Adele co-wrote the song with her producer,
+                Greg Kurstin. "Hello" is a piano ballade with soul influences (including guitar and drums), and lyrics 
+                that discuss themes of nostalgia and regret. Upon release, the song was acclaimed by music critics, who
+                compared it favourably to Adele's previous works and praised its lyrics, production and Adele's vocal
+                performance. It was recorded in London's Metropolis Studios.
+                </p>
+                <p>
+                "Hello" topped the charts in a record-breaking 36 countries, including the United Kingdom, where it became 
+                Adele's second chart topper, following "Someone Like You", and had the largest opening week sales in three
+                years. In the United States, "Hello" debuted atop the Billboard Hot 100, reigning for ten consecutive weeks
+                whilst becoming Adele's fourth number-one single on the chart and breaking several records, including
+                becoming the first song to sell over a million digital copies in a week. By the end of 2015, it had sold 
+                12.3 million units globally (combined sales and track-equivalent streams) and was the year's 7th best-selling single while it stands as one of the best-selling digital singles of all-time.
+                </p>
             </div>
     </div>    
     );
